d1a729a52cb2474a97bb1c1ea5b9984509936a82
kaushalrijal <kaushalrijal091@gmail.com>
2025-09-14 00:04:29 +0545

    add themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,18 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: {
-        template: `%s | Prostore`,
-        default: APP_NAME
-    },
-    description: APP_DESCRIPTION,
-    metadataBase: new URL(SERVER_URL)
-} 
+  title: {
+    template: `%s | Prostore`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+};
 
 export default function RootLayout({
   children,
@@ -21,10 +22,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className}antialiased`}
-      >
-        {children}
+      <body className={`${inter.className}antialiased`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
