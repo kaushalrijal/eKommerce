a298ab41f21a6058f39755446c92739c02edc10b
kaushalrijal <kaushalrijal091@gmail.com>
2025-09-14 23:40:25 +0545

    add zod intefaces and infer types

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,6 +1,7 @@
 import ProductCard from "./product-card";
+import { Product } from "@/types";
 
-const ProductList = ({ data, title, limit }: { data: any; title: string; limit?:number }) => {
+const ProductList = ({ data, title, limit }: { data: Product[]; title: string; limit?:number }) => {
     const limitedData = limit ? data.slice(0, limit) : data
   return (
     <div className="my-10">
