8fa44096eb0a9f0eb9823040cd53c344d45ea55e
kaushalrijal <kaushalrijal091@gmail.com>
2025-09-23 14:40:28 +0545

    fix jwt case error and add sign-in form components

--- auth.ts
+++ auth.ts
@@ -11,7 +11,7 @@ export const config = {
         error: '/sign-in',
     },
     session: {
-        strategy: "JWT",
+        strategy: "jwt",
         maxAge: 30 * 24 * 60 * 60, // 30 days
     },
     adapter: PrismaAdapter(prisma),
