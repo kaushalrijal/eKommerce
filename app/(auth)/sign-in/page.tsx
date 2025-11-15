25a2947a9cc3ed2c2c0ebf127612eddcc8c92db1
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-15 21:43:24 +0545

    remove unnecessary quotation mark

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -18,7 +18,7 @@ const SignInPage = () => {
                     <Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME} logo`} priority={true}/>
                 </Link>
                 <CardTitle className="text-center">Sign In</CardTitle>
-                <CardDescription className="text-center">Sign in to your account</CardDescription>"
+                <CardDescription className="text-center">Sign in to your account</CardDescription>
             </CardHeader>
             <CardContent className="space-y-4">
                 <CredentialsSignInForm />
