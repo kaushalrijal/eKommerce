bbe9ab7d567c9fe713da19bf884741b5558e8acb
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-18 18:31:12 +0545

    add dynamic cart button

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { notFound } from "next/navigation";
 import ProductPrice from "@/components/shared/product/product-price";
 import ProductImages from "@/components/product/product-images";
 import AddToCart from "@/components/shared/product/add-to-cart";
+import { getMyCart } from "@/lib/actions/cart.actions";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -15,6 +16,9 @@ const ProductDetailsPage = async (props: {
   if (!product) {
     notFound();
   }
+
+  const cart = await getMyCart();
+
   return (
     <>
       <section>
@@ -63,17 +67,20 @@ const ProductDetailsPage = async (props: {
                     <Badge variant="destructive">Out of Stock</Badge>
                   )}
                 </div>
-                {product.stock > 0 && ( 
-                    <div className="flex-center">
-                        <AddToCart item={{
-                          productId: product.id,
-                          name: product.name,
-                          slug: product.slug,
-                          price: product.price,
-                          qty: 1,
-                          image: product.images![0]
-                        }}/>
-                    </div>
+                {product.stock > 0 && (
+                  <div className="flex-center">
+                    <AddToCart
+                      cart={cart}
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        price: product.price,
+                        qty: 1,
+                        image: product.images![0],
+                      }}
+                    />
+                  </div>
                 )}
               </CardContent>
             </Card>
--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { CartItem } from "@/types";
+import { Cart, CartItem } from "@/types";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
+import { Plus, Minus } from "lucide-react";
 import { toast } from "sonner";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
     const router = useRouter();
 
     const handleAddToCart = async () => {
@@ -24,11 +24,32 @@ const AddToCart = ({ item }: { item: CartItem }) => {
             action: {
                 label: "Go to Cart",
                 onClick: () => router.push("/cart")
-            },
-            className: "bg-primary text-white hover:bg-gray-800"
+            }
         })
     }
-  return (
+
+    const handleRemoveFromCart = async () => {
+        const res = await removeItemFromCart(item.productId);
+
+        res.success ? toast.success(res.message) : toast.error(res.message);
+
+        return;
+    }
+    // check if item is in cart
+    const existItem = cart && cart.items.find((x) => x.productId === item.productId);
+
+
+  return existItem ? (
+    <div>
+        <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+            <Minus className="h-4 w-4" />
+        </Button>
+        <span className="px-2">{existItem.qty}</span>
+        <Button type="button" variant="outline" onClick={handleAddToCart}>
+            <Plus className="h-4 w-4" />
+        </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}><Plus />Add to Cart</Button>
   )
 }
