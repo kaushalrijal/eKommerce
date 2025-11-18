dbae0dfa359b4a696c9b0358a5b7edc46fde479a
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-18 18:49:51 +0545

    fix error in actions

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -141,7 +141,7 @@ export async function getMyCart() {
   return convertToPlainObject({
     ...cart,
     items: cart.items as CartItem[],
-    itmesPrice: cart.itemsPrice.toString(),
+    itemsPrice: cart.itemsPrice.toString(),
     totalPrice: cart.totalPrice.toString(),
     shippingPrice: cart.shippingPrice.toString(),
     taxPrice: cart.taxPrice.toString(),
