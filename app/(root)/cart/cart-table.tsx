cd8d284b462242e7c4bc5cf825773669ed65b70f
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-18 21:43:43 +0545

    complete cart table

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -9,6 +9,7 @@ import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Table, TableBody, TableHead, TableHeader, TableRow, TableCell } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
     const router = useRouter();
@@ -31,6 +32,50 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
                                 <TableHead className="text-right">Price</TableHead>
                             </TableRow>
                         </TableHeader>
+                        <TableBody>
+                            { cart.items.map((item)=>(
+                                <TableRow key={ item.slug}>
+                                    <TableCell>
+                                        <Link href={`/product/${item.slug}`} className="flex items-center">
+                                            <Image src={item.image} alt={item.name} width={50} height={50}/>
+                                            <span className="px-2">{item.name}</span>
+                                        </Link>
+                                    </TableCell>
+                                    <TableCell className="flex-center gap-2">
+                                        <Button disabled={isPending} variant="outline" type="button" onClick={() => startTransition(async() => {
+                                            const res = await removeItemFromCart(item.productId);
+
+                                            if(!res.success) {
+                                                toast.error(res.message);
+                                            }
+                                        })}>
+                                            { isPending ? (
+                                                <Loader className="w-4 h-4 animate-spin" /> 
+                                            ) : (
+                                                <Minus className="w-4 h-4" />
+                                            ) }
+                                        </Button>
+                                        <span>{item.qty}</span>
+                                        <Button disabled={isPending} variant="outline" type="button" onClick={() => startTransition(async() => {
+                                            const res = await addItemToCart(item);
+
+                                            if(!res.success) {
+                                                toast.error(res.message);
+                                            }
+                                        })}>
+                                            { isPending ? (
+                                                <Loader className="w-4 h-4 animate-spin" /> 
+                                            ) : (
+                                                <Plus className="w-4 h-4" />
+                                            ) }
+                                        </Button>
+                                    </TableCell>
+                                    <TableCell className="text-right">
+                                            ${item.price}
+                                    </TableCell>
+                                </TableRow>
+                            )) }
+                        </TableBody>
                     </Table>
                 </div>
             </div>
