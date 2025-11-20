fd3348f728b42bf84dd127ca3a624bf4d1afc010
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-20 21:45:09 +0545

    complete payment method form and update

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,25 +1,27 @@
-import { Metadata } from "next"
-import { auth } from "@/auth"
-import { getUserById } from "@/lib/actions/user.actions"
-import PaymentMethodForm from "./payment-method-form"
+import { Metadata } from "next";
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import PaymentMethodForm from "./payment-method-form";
+import CheckoutSteps from "@/components/shared/checkout-steps";
 
-export const metadata:Metadata = {
-    title: "Select Payment Method",
-}
+export const metadata: Metadata = {
+  title: "Select Payment Method",
+};
 
 const PaymentMethodPage = async () => {
-    const session = await auth();
-    const userId = session?.user?.id;
+  const session = await auth();
+  const userId = session?.user?.id;
 
-    if(!userId) throw new Error("User not found!");
+  if (!userId) throw new Error("User not found!");
 
-    const user = await getUserById(userId);
-    
+  const user = await getUserById(userId);
+
   return (
     <div>
-        <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+      <CheckoutSteps current={2} />
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
     </div>
-  )
-}
+  );
+};
 
-export default PaymentMethodPage
+export default PaymentMethodPage;
