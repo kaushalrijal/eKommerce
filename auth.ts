29b0b043f7987f987cc23c08f010ad6540f49f68
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-20 15:21:39 +0545

    persist session cart

--- auth.ts
+++ auth.ts
@@ -70,6 +70,7 @@ export const config = {
         async jwt({ token, user, trigger, session }: any){
             // Assign user fields to the token
             if(user){
+                token.id = user.id;
                 token.role = user.role;
 
                 // If user has no name then use the email
@@ -85,6 +86,31 @@ export const config = {
                             name: token.name
                         }
                     })
+                }
+
+                if(trigger==='signIn' || trigger==="signUp"){
+                    const cookiesObject = await cookies()
+                    const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+                    if(sessionCartId){
+                        const sessionCart = await prisma.cart.findFirst({
+                            where : {sessionCartId}
+                        })
+                        
+                        // delete current user cart
+                        if(sessionCart){
+                            await prisma.cart.deleteMany({
+                                where: { userId: user.id },
+                            })
+                            
+                            // assign new cart
+                            await prisma.cart.update({
+                                where: { id: sessionCart.id },
+                                data: { userId: user.id },
+                            })
+                        }
+
+                    }
                 }
             }
             return token;
