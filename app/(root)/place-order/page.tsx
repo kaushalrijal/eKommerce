f0f5e61172ba06871a4f39e2bf9a57c41fc4999a
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-21 22:10:59 +0545

    create place order form

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -18,6 +18,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import PlaceOrderForm from "./place-order-form";
 
 export const metadata: Metadata = {
   title: "Place Order",
@@ -136,6 +137,7 @@ const PlaceOrder = async () => {
                 <div>Total</div>
                 <div>{ formatCurrency(cart.totalPrice )}</div>
               </div>
+              <PlaceOrderForm />
             </CardContent>
           </Card>
         </div>
