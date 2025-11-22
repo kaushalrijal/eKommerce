67109f59f7175223b887c632e53c7fe0972b9735
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-22 20:25:02 +0545

    create order details table

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 import { getOrderById } from "@/lib/actions/order.actions"
 import { notFound } from "next/navigation"
 import { ShippingAddress } from "@/types"
+import OrderDetailsTable from "./order-details-table"
 
 export const metadata:Metadata = {
     title: "Order Details"
@@ -18,7 +19,10 @@ const OrderDetailsPage = async (props: {
     if(!order) notFound()
 
   return (
-    <div>Details</div>
+    <OrderDetailsTable order={{
+      ...order,
+      shippingAddress: order.shippingAddress as ShippingAddress
+    }}/>
   )
 }
 
