1cb08248aa6f303683ddba93e45235ec98e50023
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-23 11:58:37 +0545

    setup orders pagination

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -3,6 +3,7 @@ import { getMyOrders } from "@/lib/actions/order.actions";
 import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
 import Link from "next/link";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Pagination from "@/components/shared/pagination";
 
 export const metadata: Metadata = {
   title: "My Orders",
@@ -45,6 +46,11 @@ const OrdersPage = async (props: {
                 ))}
             </TableBody>
         </Table>
+        {
+            orders.totalPages >1 && (
+                <Pagination page={ Number(page) || 1 } totalPages={orders?.totalPages}/>
+            )
+        }
       </div>
     </div>
   );
