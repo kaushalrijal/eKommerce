6cfdeaa10b1d7ddfba0679471cfc0140030e994c
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-23 17:38:29 +0545

    create profile form

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Metadata } from "next"
+import { auth } from "@/auth"
+import { SessionProvider } from 'next-auth/react';
+import ProfileForm from "./profile-form";
 
-const Profile = () => {
+export const metadata:Metadata = {
+  title: "Customer Profile",
+}
+
+const Profile = async() => {
+  const session = await auth();
   return (
-    <div>Profile</div>
+    <SessionProvider session = {session}>
+      <div className="max-w-md mx-auto space-y-4">
+        <h2 className="h2-bold">Profile</h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
   )
 }
 
