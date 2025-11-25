db5c4531fd2443f3a0289c94ec75eadb297f9253
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-25 09:19:13 +0545

    fix type issues

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -5,7 +5,7 @@ import { insertProductSchema, updateProductSchema } from '@/lib/validators';
 import { Product } from "@/types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { ControllerRenderProps, useForm } from "react-hook-form";
+import { ControllerRenderProps, SubmitHandler, useForm } from "react-hook-form";
 import z from "zod";
 import {
   Form,
@@ -19,6 +19,8 @@ import slugify from "slugify";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
+import { createProduct, updateProduct } from "@/lib/actions/product.actions";
+import { toast } from "sonner";
 
 const ProductForm = ({
   type,
@@ -40,10 +42,39 @@ const ProductForm = ({
       product && type === 'Update' ? product : productDefaultValues,
   });
 
+  const onSubmit:SubmitHandler<z.infer<typeof insertProductSchema>> = async (values) => {
+    // on create
+    if (type === "Create") {
+      const res = await createProduct(values);
+
+      if(!res.success) {
+        toast.error(res.message)
+      } else {
+        toast.success(res.message);
+        router.push('/admin/products');
+      }
+    } 
+
+    // on update 
+    if (type === "Update") {
+      if(!productId){
+        router.push('/admin/products');
+        return;
+      }
+      const res = await updateProduct({...values, id: productId });
+
+      if(!res.success) {
+        toast.error(res.message)
+      } else {
+        toast.success(res.message);
+        router.push('/admin/products');
+      }
+    } 
+  }
 
   return (
     <Form {...form}>
-      <form className="space-y-8">
+      <form method="POST" onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <div className="flex flex-col gap-5 md:flex-row">
           {/* Name */}
           <FormField
