98af540f1ad0d90f3c7cb4f527c0a8ef1ace0dd8
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-25 21:28:12 +0545

    add image uploads

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -21,6 +21,9 @@ import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { createProduct, updateProduct } from "@/lib/actions/product.actions";
 import { toast } from "sonner";
+import { UploadButton } from "@/lib/uploadthing";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
 
 const ProductForm = ({
   type,
@@ -71,6 +74,8 @@ const ProductForm = ({
       }
     } 
   }
+
+  const images = form.watch('images');
 
   return (
     <Form {...form}>
@@ -218,6 +223,30 @@ const ProductForm = ({
         </div>
         <div className="flex flex-col upload-field gap-5 md:flex-row">
           {/* Images */}
+          <FormField
+            control={form.control}
+            name="images"
+            render={() => (
+              <FormItem className="w-full">
+                <FormLabel>Images</FormLabel>
+                  <Card>
+                    <CardContent className="space-y-2 mt-2 min-h-48">
+                      <div className="flex-start space-x-2">
+                        { images.map((image: string)=>(
+                          <Image src={image} key={image} alt="product image" className="w-20 h-20 object-cover object-center rounded-sm" width={100} height={100}/>
+                        ))}
+                        <UploadButton endpoint={"imageUploader"} onClientUploadComplete={(res:{ url:string }[])=>{
+                          form.setValue('images', [...images, res[0].url]);
+                        }} onUploadError={(error: Error) => {
+                          toast.error(`ERROR! ${error.message}`)
+                        }}/>
+                      </div>
+                    </CardContent>
+                  </Card>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
         <div className="upload-field">{/* isFeatured */}</div>
         <div>
