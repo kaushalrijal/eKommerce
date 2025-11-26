7d52134eb913d33c70b884b7cfb5965c36047d7a
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-26 22:41:24 +0545

    add products filter

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -5,6 +5,7 @@ import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
 import z from "zod";
 import { insertProductSchema, updateProductSchema } from "../validators";
+import { Prisma } from "../generated/prisma";
 
 // Get latest products
 export async function getLatestProducts(){
@@ -45,7 +46,18 @@ export async function getAllProducts({
     page: number;
     category?: string;
 }) {
+    // Query filter
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== 'all'
+      ? {
+          name: {
+            contains: query,
+            mode: 'insensitive',
+          } as Prisma.StringFilter,
+        }
+      : {};
     const data = await prisma.product.findMany({
+        where: {...queryFilter},
         orderBy: {createdAt: "desc"},
         skip: (page - 1) * limit,
         take: limit
