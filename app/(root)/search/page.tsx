156e10b004910c7ff123703aa93c04042ba6c668
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-27 13:17:23 +0545

    add search component

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next"
+
+const metadata: Metadata = {
+    title: "Search Results"
+}
+
+const SearchPage = () => {
+  return (
+    <div>Search Results</div>
+  )
+}
+
+export default SearchPage
