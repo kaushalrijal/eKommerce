47443b0241cbadc8730554fa8f7df70af01d5713
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-28 13:07:18 +0545

    sorting products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -81,7 +81,13 @@ export async function getAllProducts({
     } : {}
   const data = await prisma.product.findMany({
     where: { ...queryFilter, ...categoryFilter, ...priceFilter, ...ratingFilter },
-    orderBy: { createdAt: "desc" },
+    orderBy: sort==="lowest"
+      ? { price : "asc"}
+      : sort=== "hightest"
+        ? { price: "desc"}
+        : sort === "rating"
+          ? {rating: "desc"}
+          : {createdAt: "desc"},
     skip: (page - 1) * limit,
     take: limit,
   });
