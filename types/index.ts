32e25a28b9c4503d29381d61709e07c3921503c4
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-30 22:10:46 +0545

    display reviews

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,7 @@ import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressS
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
     rating: string;
+    numReviews: number;
     createdAt: Date;
 }
 
