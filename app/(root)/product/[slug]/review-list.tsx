2874110a44587a44241de597dd57475d3d2a6975
kaushalrijal <kaushalrijal091@gmail.com>
2025-11-30 21:38:14 +0545

    create review form

--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -3,6 +3,7 @@
 import { Review } from "@/types";
 import Link from "next/link";
 import { useState } from "react";
+import ReviewForm from "./review-form";
 
 const ReviewList = ({
   userId,
@@ -18,7 +19,7 @@ const ReviewList = ({
     <div className="space-y-4">
       {reviews.length === 0 && <div>No reviews yet</div>}
       {userId ? (
-        <>{/* REVIEW FORM HERE */}</>
+        <ReviewForm userId={userId} productId={productId} />
       ) : (
         <div>
           Please
