fd02b6461b951eaf00efd2246fec5b02739fc8ed
kaushalrijal <kaushalrijal091@gmail.com>
2025-12-01 18:56:58 +0545

    setup resend main function

---
+++ email/purchase-receipt.tsx
@@ -0,0 +1,12 @@
+import { Order } from '@/types'
+import React from 'react'
+
+const PurchaseRecepitEmail = ({ order }: {
+    order: Order
+}) => {
+  return (
+    <div>EMAIL</div>
+  )
+}
+
+export default PurchaseRecepitEmail
