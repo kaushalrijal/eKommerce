d44d8f8f7b5d32f647033e152e8063dc4d479723
kaushalrijal <kaushalrijal091@gmail.com>
2025-12-02 13:16:55 +0545

    create icon boxes components for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,26 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
 
 const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
-    const latestProducts = await getLatestProducts();
-    const featuredProducts = await getFeaturedProducts();
-    
+  return (
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+      <ViewAllProductsButton />
+      <IconBoxes />
+    </>
+  );
+};
 
-    return ( 
-        <>
-        {featuredProducts.length>0 && <ProductCarousel data={featuredProducts} />}
-        <ProductList data={latestProducts} title="Newest Arrivals" limit={4}/>
-        <ViewAllProductsButton />
-        </>
-     );
-}
- 
 export default Homepage;
