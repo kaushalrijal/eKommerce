0886cebb58f607833a523d904c58d3440d2eb6e8
kaushalrijal <kaushalrijal091@gmail.com>
2025-12-02 10:58:53 +0545

    fix email issue

--- email/index.tsx
+++ email/index.tsx
@@ -1,17 +1,23 @@
 import {Resend} from 'resend';
+import { render } from '@react-email/render';
 import { SENDER_EMAIL, APP_NAME } from '@/lib/constants';
 import { Order } from '@/types';
 import PurchaseReceiptEmail from "./purchase-receipt";
+import React from 'react';
 
 require('dotenv').config();
 
 const resend = new Resend(process.env.RESEND_API_KEY as string);
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order;}) => {
+    const emailHtml = await render(
+        React.createElement(PurchaseReceiptEmail, { order })
+    );
+    
     await resend.emails.send({
         from: `${APP_NAME} <${SENDER_EMAIL}>`,
         to: order.user.email,
         subject: `Order Confirmation ${order.id}`,
-        react: <PurchaseReceiptEmail order={order} />
+        html: emailHtml
     })
 }
